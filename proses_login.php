a03848c85cae0bbde2fcd32772b2630ebe5e2bdb
sabian77 <sabianrakasinau@gmail.com>
2024-03-22 08:42:04 +0700

    komit

--- proses_login.php
+++ proses_login.php
@@ -10,12 +10,12 @@ $query = "SELECT * FROM siswa WHERE username='$username' AND password='$password
 $result = mysqli_query($is_connect, $query);
 
 if(mysqli_num_rows($result) > 0){
-    session_start();
-    $_SESSION['login'] = true;
+    $_SESSION['id'] = $data['id'];
     $_SESSION['username'] = $username;
     header('Location: index.php');
 } else {
     echo 'Login gagal cek kembali username atau password';
+    echo "<a href='login.php'>kembali</a>";
 }
 
 ?>
